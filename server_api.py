72bcebd2a436f4ad653eb074ec4d5597a0543417
Sonyy172 <phuc.nh94@gmail.com>
2017-09-23 11:11:10 +0700

    a

--- server_api.py
+++ server_api.py
@@ -95,7 +95,7 @@ def get_all_news():
     output = []
     for news in news.find():
         output.append({
-            'id': news._id,
+            'id': news.next()._id,
             'title': news['title'],
             'subtitle': news['subtitle'],
             'image_url': news['image_url'],
